8e4986d1c378dc864979c59efe9b974244829758
withshk <withshk0527@gmail.com>
2026-07-28 11:03:40 +0900

    initial commit

---
+++ src/main/java/LINX/linx/LinxApplication.java
@@ -0,0 +1,13 @@
+package LINX.linx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinxApplication.class, args);
+	}
+
+}
