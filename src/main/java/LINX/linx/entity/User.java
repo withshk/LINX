562657f9a2cebcfc7166b77dd4ce2a51f47035bf
withshk <withshk0527@gmail.com>
2026-08-07 08:32:00 +0900

    fix: user 엔티티 중 nickname의 unique 삭제

--- src/main/java/LINX/linx/entity/User.java
+++ src/main/java/LINX/linx/entity/User.java
@@ -22,7 +22,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String nickname;
 
     private String profileImageUrl;
