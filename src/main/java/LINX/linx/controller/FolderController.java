5e852d4362c6431abc67bc2f62b68f448acdf14b
withshk <withshk0527@gmail.com>
2026-08-12 18:08:09 +0900

    Fix: 빠져있던 폴더 컨트롤러 추가

---
+++ src/main/java/LINX/linx/controller/FolderController.java
@@ -0,0 +1,26 @@
+package LINX.linx.controller;
+
+import LINX.linx.dto.FolderResponse;
+import LINX.linx.service.FolderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/folders")
+public class FolderController {
+
+    private final FolderService folderService;
+
+    public FolderController(FolderService folderService){
+        this.folderService = folderService;
+    }
+
+    @GetMapping
+    public List<FolderResponse> getAllFolders() {
+        return folderService.getAllFolders();
+    }
+
+}
