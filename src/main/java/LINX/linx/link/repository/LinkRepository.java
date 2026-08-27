73e97e1d02070ebe7c55cc0a0494abcde9592f2a
withshk <withshk0527@gmail.com>
2026-08-27 15:05:11 +0900

    feat: 고정 링크 조회 기능 추가

--- src/main/java/LINX/linx/link/repository/LinkRepository.java
+++ src/main/java/LINX/linx/link/repository/LinkRepository.java
@@ -3,5 +3,9 @@ package LINX.linx.link.repository;
 import LINX.linx.link.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LinkRepository extends JpaRepository<Link, Long> {
+    List<Link> findByIsPinned(Boolean pinned);
 }
