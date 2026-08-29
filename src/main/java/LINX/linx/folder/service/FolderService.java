439b2c34a75af5f9deb8fbb3cc0af54dc80e54c7
withshk <withshk0527@gmail.com>
2026-08-29 16:37:18 +0900

    Fix: 지예아 폴더 안에 링크 잇으면 삭제 안되는 버그 싹 수정해버리는주현욱수준이존나놀랍다

--- src/main/java/LINX/linx/folder/service/FolderService.java
+++ src/main/java/LINX/linx/folder/service/FolderService.java
@@ -5,6 +5,8 @@ import LINX.linx.folder.Folder;
 import LINX.linx.folder.dto.request.FolderRequest;
 import LINX.linx.folder.dto.response.FolderResponse;
 import LINX.linx.folder.repository.FolderRepository;
+import LINX.linx.link.Link;
+import LINX.linx.link.repository.LinkRepository;
 import LINX.linx.user.User;
 import LINX.linx.user.repository.UserRepository;
 import org.springframework.http.HttpStatus;
@@ -17,10 +19,12 @@ public class FolderService {
 
     private final FolderRepository folderRepository;
     private final UserRepository userRepository;
+    private final LinkRepository linkRepository;
 
-    public FolderService(FolderRepository folderRepository, UserRepository userRepository) {
+    public FolderService(FolderRepository folderRepository, UserRepository userRepository, LinkRepository linkRepository) {
         this.folderRepository = folderRepository;
         this.userRepository = userRepository;
+        this.linkRepository = linkRepository;
     }
 
     public List<FolderResponse> getAllFolders() {
@@ -45,6 +49,9 @@ public class FolderService {
     public void deleteFolder(Long id) {
         Folder folder = folderRepository.findById(id)
                 .orElseThrow(() -> new CustomException("FOLDER_NOT_FOUND", HttpStatus.NOT_FOUND, "존재하지 않는 폴더입니다."));
+
+        List<Link> links = linkRepository.findByFolderId(id);
+        links.forEach(Link::removeFromFolder);
 
         folderRepository.delete(folder);
     }
--- src/main/java/LINX/linx/link/repository/LinkRepository.java
+++ src/main/java/LINX/linx/link/repository/LinkRepository.java
@@ -1,5 +1,6 @@
 package LINX.linx.link.repository;
 
+import LINX.linx.folder.Folder;
 import LINX.linx.link.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,5 @@ import java.util.Optional;
 
 public interface LinkRepository extends JpaRepository<Link, Long> {
     List<Link> findByIsPinned(Boolean pinned);
+    List<Link> findByFolderId(Long folderId);
 }
